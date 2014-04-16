cfd59e9a1c94865d0aa69ce11b4a367456572b67
Nicolas Barroso <nicolasbarroso@hotmail.fr>
2014-04-16 17:55:11 +0200

    Animation de la navigation

---
+++ www/js/controller/memCtrl.js
@@ -0,0 +1,9 @@
+function memCtrl($scope){
+	$scope.pageLevel = 0;
+	console.log("page level : " + $scope.pageLevel);
+
+	$scope.increaseLevel = function() {
+    $scope.pageLevel ++;
+    console.log("page level après click : " + $scope.pageLevel);
+  };
+}
