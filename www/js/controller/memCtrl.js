2c0dbbaebe3f30747e675e7ded17360fae71b73a
RomaneBorgeais <Romane@MacBook-Pro-de-Romane.local>
2014-05-19 09:17:54 +0200

    new

--- www/js/controller/memCtrl.js
+++ www/js/controller/memCtrl.js
@@ -1,9 +1,9 @@
 function memCtrl($scope){
 	$scope.pageLevel = 0;
-	console.log("page level : " + $scope.pageLevel);
+	//console.log("page level : " + $scope.pageLevel);
 
 	$scope.increaseLevel = function() {
     $scope.pageLevel ++;
-    console.log("page level après click : " + $scope.pageLevel);
+    //console.log("page level après click : " + $scope.pageLevel);
   };
 }
