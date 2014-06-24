e8ca040359770ecf1e00427e60177298c40d051b
Nicolas Barroso <nicolasbarroso@hotmail.fr>
2014-06-24 14:17:11 +0200

    font-icon + modifications less

--- www/js/app.js
+++ www/js/app.js
@@ -1,120 +1,43 @@
-document.addEventListener('deviceready', function() {
+document.addEventListener('deviceready', function(){
 
-    //Ici du code au lancement de l'appli
+	//Ici du code au lancement de l'appli
 
 }, false);
 
 var app = angular.module('app', []);
 
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/intro', {templateUrl: 'partials/intro.html'})
 
-app.directive('jqdatepicker', function() {
-    return {
-        restrict: 'A',
-        require: 'ngModel',
-        link: function(scope, element, attrs, ngModelCtrl) {
-            element.datepicker({
-                dateFormat: 'yy-mm-dd',
-                onSelect: function(date) {
-                    scope.date = date;
-                    scope.$apply();
-                }
-            });
-        }
-    };
-});
+		.when('/home', {templateUrl: 'partials/home.html'})
+		.when('/sidebar', {templateUrl: 'partials/sidebar.html'})
 
-app.config(function($routeProvider) {
-    $routeProvider
-        .when('/intro', {
-            templateUrl: 'partials/intro.html'
-        })
+		.when('/memoriser', {templateUrl: 'partials/memoriser.html'})
+		.when('/vocal', {templateUrl: 'partials/vocal.html'})
+		.when('/vocal_2', {templateUrl: 'partials/vocal_2.html'})
+		.when('/vocal_3', {templateUrl: 'partials/vocal_3.html'})
+		.when('/vocal_4', {templateUrl: 'partials/vocal_4.html'})
+		.when('/vocal_5', {templateUrl: 'partials/vocal_5.html'})
 
-    .when('/home', {
-        templateUrl: 'partials/home.html'
-    })
-        .when('/sidebar', {
-            templateUrl: 'partials/sidebar.html'
-        })
+		.when('/questions_date', {templateUrl: 'partials/questions_date.html'})
+		.when('/questions_type', {templateUrl: 'partials/questions_type.html'})
+		.when('/questions_couleur', {templateUrl: 'partials/questions_couleur.html'})
+		.when('/questions_emotion', {templateUrl: 'partials/questions_emotion.html'})
+		.when('/questions_lieu', {templateUrl: 'partials/questions_lieu.html'})
+		.when('/questions_personnage', {templateUrl: 'partials/questions_personnage.html'})
+		.when('/questions_personnage_humain', {templateUrl: 'partials/questions_personnage_humain.html'})
+		.when('/questions_personnage_animal', {templateUrl: 'partials/questions_personnage_animal.html'})
+		.when('/questions_personnage_autre', {templateUrl: 'partials/questions_personnage_autre.html'})
+		.when('/questions_action', {templateUrl: 'partials/questions_action.html'})
+		.when('/questions_keywords', {templateUrl: 'partials/questions_keywords.html'})
+		.when('/questions_titre', {templateUrl: 'partials/questions_titre.html'})
 
-    .when('/memoriser', {
-        templateUrl: 'partials/memoriser.html'
-    })
-        .when('/vocal', {
-            templateUrl: 'partials/vocal.html'
-        })
-        .when('/vocal_2', {
-            templateUrl: 'partials/vocal_2.html'
-        })
-        .when('/vocal_3', {
-            templateUrl: 'partials/vocal_3.html'
-        })
-        .when('/vocal_4', {
-            templateUrl: 'partials/vocal_4.html'
-        })
-        .when('/vocal_5', {
-            templateUrl: 'partials/vocal_5.html'
-        })
+		.when('/explorer', {templateUrl: 'partials/explorer.html'})
 
-    .when('/questions_date', {
-        templateUrl: 'partials/questions_date.html',
-        controller: 'memCtrl'
-    })
-        .when('/questions_type', {
-            templateUrl: 'partials/questions_type.html',
-            controller: 'memCtrl'
-        })
-        .when('/questions_couleur', {
-            templateUrl: 'partials/questions_couleur.html',
-            controller: 'memCtrl'
-        })
-        .when('/questions_emotion', {
-            templateUrl: 'partials/questions_emotion.html',
-            controller: 'memCtrl'
-        })
-        .when('/questions_lieu', {
-            templateUrl: 'partials/questions_lieu.html',
-            controller: 'memCtrl'
-        })
-        .when('/questions_personnage', {
-            templateUrl: 'partials/questions_personnage.html',
-            controller: 'memCtrl'
-        })
-        .when('/questions_personnage_humain', {
-            templateUrl: 'partials/questions_personnage_humain.html',
-            controller: 'memCtrl'
-        })
-        .when('/questions_personnage_animal', {
-            templateUrl: 'partials/questions_personnage_animal.html',
-            controller: 'memCtrl'
-        })
-        .when('/questions_personnage_autre', {
-            templateUrl: 'partials/questions_personnage_autre.html',
-            controller: 'memCtrl'
-        })
-        .when('/questions_action', {
-            templateUrl: 'partials/questions_action.html',
-            controller: 'memCtrl'
-        })
-        .when('/questions_keywords', {
-            templateUrl: 'partials/questions_keywords.html',
-            controller: 'memCtrl'
-        })
-        .when('/questions_titre', {
-            templateUrl: 'partials/questions_titre.html',
-            controller: 'memCtrl'
-        })
+		.when('/visualiser', {templateUrl: 'partials/visualiser.html'})
 
+		.when('/partager', {templateUrl: 'partials/partager.html'})
 
-
-    .when('/visualiser', {
-        templateUrl: 'partials/visualiser.html'
-    })
-
-    .when('/partager', {
-        templateUrl: 'partials/partager.html'
-    })
-
-    .otherwise({
-        redirectTo: '/home'
-    })
+		.otherwise({redirectTo: '/home'})
 })
--- www/js/controller/memCtrl.js
+++ www/js/controller/memCtrl.js
@@ -1,65 +1,9 @@
-function memCtrl($scope) {
-    $scope.pageLevel = 0;
-    //console.log("page level : " + $scope.pageLevel);
+function memCtrl($scope){
+	$scope.pageLevel = 0;
+	//console.log("page level : " + $scope.pageLevel);
 
-    $scope.increaseLevel = function() {
-        $scope.pageLevel++;
-        console.log($scope.pageLevel);
-        //console.log("page level après click : " + $scope.pageLevel);
-    };
-
-    //$scope.date = 0;
-    //$scope.type = 0;
-    //$scope.titre = 0;
-
-    $scope.addDate = function(date) {
-        $scope.$parent.date = date;
-        console.log($scope.$parent.date + " // " + $scope.$parent.type + " // " + $scope.$parent.titre);
-    };
-
-    $scope.addType = function(type) {
-        $scope.$parent.type = type;
-        console.log($scope.$parent.date + " // " + $scope.$parent.type + " // " + $scope.$parent.titre);
-    };
-
-    $scope.addTitre = function(titre) {
-        $scope.$parent.titre = titre;
-        console.log($scope.$parent.date + " // " + $scope.$parent.type + " // " + $scope.$parent.titre);
-    };
-
-    $scope.postReve = function() {
-
-        console.log($scope.date + " // " + $scope.type + " // " + $scope.titre);
-        if ($scope.date != null && $scope.type != null && $scope.titre != null) {
-
-            var date_reve = $scope.$parent.date;
-            var type_reve = $scope.$parent.type;
-            var titre_reve = $scope.$parent.titre;
-
-            jQuery.ajax({
-
-                type: "GET",
-                url: "http://dreamorize.meximas.com/back_office/app/insertFromMobile.php",
-                data: {
-                    date: date_reve,
-                    type: type_reve,
-                    titre: titre_reve
-                },
-                error: function(xhr, status, error) {
-                    alert("erreur");
-                },
-                success: function(data, status, XHR) {
-                    console.log("requête réussie");
-                    console.log(data['responseText']);
-                }
-            });
-        } else {
-            alert("Variables de rêve méconnues.");
-        }
-
-        $scope.$parent.date = "";
-        $scope.$parent.type = "";
-        $scope.$parent.titre = "";
-
-    }
+	$scope.increaseLevel = function() {
+    $scope.pageLevel ++;
+    //console.log("page level après click : " + $scope.pageLevel);
+  };
 }
